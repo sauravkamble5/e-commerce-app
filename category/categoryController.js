fd429a635300665700483638deeaada7ac24fe1c
saurav <sauravkamble.dev@gmail.com>
2024-08-16 09:57:54 +0530

    add createCategory,getAllCategories,deleteCategory,updateCategory

--- category/categoryController.js
+++ category/categoryController.js
@@ -1,5 +1,6 @@
 import createError from "http-errors";
 import CategoryModel from "./categoryModel.js";
+import ProductModel from "../product/productModel.js";
 
 export const createCategory = async (req, res, next) => {
   try {
@@ -37,3 +38,72 @@ export const getAllCategories = async (req, res, next) => {
     next(createError(500, "Internal server error"));
   }
 };
+
+export const deleteCategory = async (req, res, next) => {
+  try {
+    const categoryId = req.params.id;
+    if (!categoryId) {
+      return next(createError(400, "Category ID is required"));
+    }
+    const category = await CategoryModel.findById(categoryId);
+    if (!category) {
+      return next(createError(404, "Category not found"));
+    }
+
+    const products = await ProductModel.find({ category: category._id });
+
+    for (let i = 0; i < products.length; i++) {
+      const product = products[i];
+      product.category = undefined;
+      await product.save();
+    }
+    await category.deleteOne();
+
+    res.status(200).send({
+      status: true,
+      message: `Category ${categoryId} deleted successfully`,
+    });
+  } catch (error) {
+    console.error("Error deleting category", error.stack || error.message);
+    next(createError(500, "Internal server error"));
+  }
+};
+
+export const updateCategory = async (req, res, next) => {
+  try {
+    const categoryId = req.params.id;
+    if (!categoryId) {
+      return next(createError(400, "Category ID is required"));
+    }
+
+    const category = await CategoryModel.findById(categoryId);
+    if (!category) {
+      return next(createError(404, "Category not found"));
+    }
+    const { updatedCategory } = req.body;
+
+    category.category = updatedCategory;
+
+    await category.save();
+
+    const products = await ProductModel.find({ category: category._id });
+
+    for (let i = 0; i < products.length; i++) {
+      const product = products[i];
+      product.category = updatedCategory;
+      await product.save();
+    }
+
+    res.status(200).send({
+      status: true,
+      message: `Category ${categoryId} updated successfully`,
+      data: updatedCategory,
+      products,
+    });
+  } catch (error) {
+    console.error("Error updating category", error.stack || error.message);
+    next(createError(500, "Internal server error"));
+  }
+};
+
+ProductModel.updateMany()
--- category/categoryRoute.js
+++ category/categoryRoute.js
@@ -1,10 +1,19 @@
 import express from "express";
 const router = express.Router();
 import { isAuth } from "../middleware/authMiddleware.js";
-import { createCategory, getAllCategories } from "./categoryController.js";
+import {
+  createCategory,
+  deleteCategory,
+  getAllCategories,
+  updateCategory,
+} from "./categoryController.js";
 
 router.post("/createCategory", isAuth, createCategory);
 
-router.get('/getAllCategories', getAllCategories)
+router.get("/getAllCategories", getAllCategories);
+
+router.delete("/deleteCategory/:id", isAuth, deleteCategory);
+
+router.put("/updateCategory/:id", isAuth, updateCategory);
 
 export default router;
